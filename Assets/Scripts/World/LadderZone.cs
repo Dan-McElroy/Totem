c5391d832bc9930f1743315c51561f3383902ede
Dan McElroy <daniel@mcelroys.net>
2016-01-30 10:22:45 +0000

    Added custom player scripts
    Also, the beginning of ladder logic.

---
+++ Assets/Scripts/World/LadderZone.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Scripts.Player;
+
+namespace Scripts
+{
+    public class LadderZone : MonoBehaviour
+    {
+
+        private PlatformerCharacter2D Player;
+
+        // Use this for initialization
+        void Start()
+        {
+            Player = FindObjectOfType<PlatformerCharacter2D>();
+        }
+
+        void OnTriggerEnter2D (Collider2D otherCollider)
+        {
+            if (otherCollider.name == "Player")
+            {
+                Player.OnLadder = true;
+            }
+        }
+    }
+}
