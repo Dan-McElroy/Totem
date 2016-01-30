2e033b802c2eb520f3514fd3e182e92813e26c2b
Dan McElroy <daniel@mcelroys.net>
2016-01-30 13:58:35 +0000

    Added basic level-end logic.
    Note: will not work until we have actual scenes.

---
+++ Assets/Scripts/World/LevelEnd.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Scripts.World
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class LevelEnd : MonoBehaviour
+    {
+        private int m_LevelIndex = 0;
+
+        void OnTriggerEnter2D(Collider2D collider)
+        {
+            if (collider.name == "Player")
+            {
+                // End Level
+                SceneManager.LoadScene(m_LevelIndex);
+            }
+        }
+    }
+}
